0a72f75987b91fcd86b7cfb04348704f9435fac7
kdrv dev <ffeodalik@gmail.com>
2024-10-15 14:44:36 +0500

    ui

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+let hide = document.querySelector(".hide")
+let hideButton = document.querySelector(".category")
+
+
+hideButton.addEventListener("click",function(){
+    hide.classList.toggle("top")
+
+})
